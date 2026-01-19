dc729678dd85043a6632441515e12d84d5bb5e6e
caelykk <caelykkaty.m@gmail.com>
2026-01-19 15:06:52 +0300

    upd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 "use strict";
 
+/*
+let name = prompt("What's your name?", '');
+alert(name);
 
-// let name = prompt("What's your name?", '');
-// alert(name);
+confirm(`Your name is ${name}`);
+*/
 
-// confirm(`Your name is ${name}`); 
-
+/*
 let boolValue = true;
 boolValue = String(boolValue);
 
@@ -15,52 +17,32 @@ let str = '123';
 str = Number(str);
 
 let strToNum = '32';
+*/
 
-
+/*
 console.log(`${str} - ${typeof(str)}`)
 console.log(`${strToNum} - ${typeof(strToNum)}`)
 strToNum = Number(strToNum);
 console.log(`${strToNum} - ${typeof(strToNum)}`)
+*/
+
+console.log("" + 1 + 0); // "10"
+console.log("" - 1 + 0); // -1
+console.log(true + false); // true or 1
+console.log(6 / "3"); // 2
+console.log("2" * "3"); // 6
+console.log(4 + 5 + "px"); // "9px"
+console.log("$" + 4 + 5); // "$45"
+console.log("4" - 2); // 2
+console.log("4px" - 2); // NaN
+console.log("  -9  " + 5); // "  -9  5"
+console.log("  -9  " - 5); // -14
+console.log(null + 1); // 1
+console.log(undefined + 1); // NaN
+console.log(" \t \n" - 2); // -2
 
 
-// alert( alert(1) && alert(2) );
-
-let login = prompt("login", '');
-if (login === "admin") {
-    let pass = prompt("psassword", '');
-    if (pass) {
-        if (pass === "Я главный") {
-            alert("Здравствуйте");
-        } else {
-            alert("Неверный пароль");
-        }
-    } else {
-        alert("Отменено");
-    }
-} else {
-    alert("Я вас не знаю");
-}
-
-if (pass > 19 && age <= 90) {
-    alert(age);
-}
-
-// "" + 1 + 0 // "10"
-// "" - 1 + 0 // -1
-// true + false // true or 1
-// 6 / "3" // 2
-// "2" * "3" // 6
-// 4 + 5 + "px" // "9px"
-// "$" + 4 + 5 // "$45"
-// "4" - 2 // 2
-// "4px" - 2 // NaN
-// "  -9  " + 5 // "  -9  5"
-// "  -9  " - 5 // -14
-// null + 1 // 1
-// undefined + 1 // NaN
-// " \t \n" - 2 // -2
-
-||= - "Логическое присваивание ИЛИ" - 
-&&= - "Логическое присваивание И"
-??= - "Оператор нулевого слияния" - 'Возвращает первое определенное значение'
-??= - "Оператор нулевого присваивания" - 'Причваивает первое определенное значение'
+a ||= b; // "Логическое присваивание ИЛИ" -
+a &&= b; // "Логическое присваивание И"
+a ?? b; // 'Возвращает первое определенное значение'
+a ??= b; // 'Приcваивает первое определенное значение'
