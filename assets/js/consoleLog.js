e84bc252af42b341735e87a1c3010bcf1a0bc9d0
caelykk <caelykkaty.m@gmail.com>
2026-01-20 12:54:02 +0300

    upd

---
+++ assets/js/consoleLog.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!"); // Вывод в консоль
+
+
+console.log("Hello, World!");
---
+++ assets/js/modals.js
@@ -0,0 +1,4 @@
+alert("Hello!");
+let a = prompt("What's your name?", ''); // Возвращает введенный текст или null
+let b = confirm("Your name is " + a); // Возвращает true или false
+
